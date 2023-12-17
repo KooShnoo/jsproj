b9afe8780fc9c30fdc188fb7a57ad7b3b2b78bf1
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-17 10:29:09 -0500

    polish test build

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,24 @@ import tsetSpec from '@exercism/javascript/exercises/practice/hello-world/hello-
 import tset from '@exercism/javascript/exercises/practice/hello-world/hello-world?raw';
 import instructions from '@exercism/javascript/exercises/practice/hello-world/.docs/instructions.md?raw';
 import testCode from './test/test';
+import './three/scene'
 
-const editor = monaco.editor.create(document.getElementById('container')!, {
-  value: tset,
+const devMessage = '// open the console for instructions and feedback! :)\n// this is just a test right now.\n// thanks to excercism for the coding problems!\n\n';
+const editor = monaco.editor.create(document.getElementById('editor')!, {
+  value: devMessage + tset,
   language: 'javascript',
   automaticLayout: true,
   theme: 'vs-dark',
 });
 
+console.log('Other problems: ' + manifest.exercises.practice.map( problem => problem.name ).join(', '));
 console.log(instructions);
-console.log(manifest.exercises.practice.map( problem => problem.slug ));
 
 const onWriteCode = async () => {
   const report = await testCode(editor.getValue(), tsetSpec);
-  console.log(report.specs().map(spec => spec.failedExpectations.map (failure => failure.message)).join('\n'));
+  const failures = report.specs().map(spec => spec.failedExpectations.map (failure => failure.message)).join('\n');
+  if (!failures) alert('you passed! 🥳\nunfortunately there\'s nothing much for you to do now.\nmaybe implement a feature yourself and submit a PR?')
+  console.log( failures || 'yay, you did it! :)');
 };
 
 editor.onDidChangeModelContent(onWriteCode);
